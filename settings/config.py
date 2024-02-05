5e5a81bfc138a8c9fefeb7a88be99f5b9464a1f6
AzaZLO <MaloyMeee@yandex.ru>
2024-02-06 01:41:31 +0300

    add typehint

--- settings/config.py
+++ settings/config.py
@@ -1,16 +1,17 @@
+import hexbytes.main
 from web3 import Web3
 
 RPC_URL = ""
 ADMIN_USER_ID = ""
 TG_BOT_KEY = ""
 WALLET_ADDR = ""
-DATABASE_FILE = "faucet.db"
+DATABASE_FILE = ""
 MNEMONIC = ""
 
 web3 = Web3(Web3.HTTPProvider(RPC_URL))
 
 
-def get_private():
+def get_private() -> hexbytes.main.HexBytes:
     web3.eth.account.enable_unaudited_hdwallet_features()
     account = web3.eth.account.from_mnemonic(MNEMONIC)
     private_key = account._private_key
